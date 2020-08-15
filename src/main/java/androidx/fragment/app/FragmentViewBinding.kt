84b8bc8d41097c883ecddf82d0a8d5d890aa1d25
Simon Norberg <simon.norberg@gmail.com>
2020-08-15 18:22:12 +0200

    Kotlin 1.4

--- src/main/java/androidx/fragment/app/FragmentViewBinding.kt
+++ src/main/java/androidx/fragment/app/FragmentViewBinding.kt
@@ -5,7 +5,6 @@ import androidx.annotation.MainThread
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.observe
 import androidx.viewbinding.ViewBinding
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
